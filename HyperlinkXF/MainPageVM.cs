9047c123fcc7c8d8890d77182f26328f9cda87be
Arseniy Zorin <arsenchozzz@hotmail.com>
2022-01-25 16:47:08 +0300

    Add project files.

---
+++ HyperlinkXF/MainPageVM.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Windows.Input;
+
+using Xamarin.Forms;
+
+namespace HyperlinkXF
+{
+    internal class MainPageVM : INotifyPropertyChanged
+    {
+        public ICommand ClickCommand => new Command<string>((url) =>
+        {
+            Device.OpenUri(new System.Uri(url));
+        });
+
+        public MainPageVM()
+        {
+
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
